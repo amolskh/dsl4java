4904a32d5de4179e324cc2907174a7ba7f32ec14
amolk <amolskh@gmail.com>
2013-07-23 03:43:03 +0530

    Initial Release

---
+++ SampleProject/src/main/java/org/sample/App.java
@@ -0,0 +1,12 @@
+package org.sample;
+
+import org.dsl.Initialise.InitDSL;
+
+public class App 
+{
+    public static void main( String[] args ) throws InterruptedException
+    {
+    	InitDSL.initialise();
+       
+    }
+}
---
+++ SampleProject/src/main/resources/demo.dsl
@@ -0,0 +1,5 @@
+Open http://127.0.0.1/wordpress/wp-admin
+Type admin in element with id = user_login
+Type test in element with id = user_pass
+Click element with id = wp-submit
+QuitBrowser
